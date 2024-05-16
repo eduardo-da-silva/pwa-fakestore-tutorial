387c6c6bb86d6b186de0e978c9f17dd09cad2cfd
Eduardo da Silva <eduardo.silva@gmail.com>
2024-05-16 16:32:51 -0300

    Update Firebase SDK imports

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,32 +1,32 @@
-// importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js')
-// importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js')
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js')
+importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js')
 
-// firebase.initializeApp({
-//   apiKey: 'AIzaSyAVfvqj1wUrjRpdlRoWc_fep474lG2-5Ms',
-//   authDomain: 'fakestore-tutorial.firebaseapp.com',
-//   projectId: 'faketore-tutosrial',
-//   storageBucket: 'fakestore-tutorial.appspot.com',
-//   messagingSenderId: '341042937156',
-//   appId: '1:341042937156:web:722a1402ccf2b0d09b2b11',
-//   measurementId: 'G-NHRFLWXHPC'
-// })
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js'
-import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-analytics.js'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+firebase.initializeApp({
   apiKey: 'AIzaSyAVfvqj1wUrjRpdlRoWc_fep474lG2-5Ms',
   authDomain: 'fakestore-tutorial.firebaseapp.com',
-  projectId: 'fakestore-tutorial',
+  projectId: 'faketore-tutosrial',
   storageBucket: 'fakestore-tutorial.appspot.com',
   messagingSenderId: '341042937156',
   appId: '1:341042937156:web:722a1402ccf2b0d09b2b11',
   measurementId: 'G-NHRFLWXHPC'
-}
+})
+// import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js'
+// import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-analytics.js'
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyAVfvqj1wUrjRpdlRoWc_fep474lG2-5Ms',
+//   authDomain: 'fakestore-tutorial.firebaseapp.com',
+//   projectId: 'fakestore-tutorial',
+//   storageBucket: 'fakestore-tutorial.appspot.com',
+//   messagingSenderId: '341042937156',
+//   appId: '1:341042937156:web:722a1402ccf2b0d09b2b11',
+//   measurementId: 'G-NHRFLWXHPC'
+// }
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig)
+// const analytics = getAnalytics(app)
