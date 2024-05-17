488f89d356ce20aca3d6441accc684ea71b949c7
Eduardo da Silva <eduardo.silva@gmail.com>
2024-05-17 17:05:24 -0300

    Remove commented code and unused imports

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,6 @@ import { messaging } from '@/plugins/firebase'
 
 import { useMessagingStore } from '@/stores/messaging'
 const messagingStore = useMessagingStore()
-// const vapidKey = import.meta.env.VITE_VAPID_KEY
-
-// let token = ref();
 
 onMessage(messaging, (payload) => {
   navigator.serviceWorker.ready.then((registration) => {
@@ -19,22 +16,6 @@ onMessage(messaging, (payload) => {
   });
 });
 
-// const fetchToken = async () => {
-//   try {
-//     const currentToken = await getToken(messaging, { vapidKey })
-//     if (currentToken) {
-//       token.value = currentToken;
-//       // Send the token to your server and update the UI if necessary
-//     } else {
-//       console.log('No registration token available. Request permission to generate one.');
-//     }
-//   }
-//   catch (err) {
-//     console.log('An error occurred while retrieving token. ', err);
-//     err.value = err
-//   }
-// }
-
 onMounted(() => {
   messagingStore.getToken()
 })
@@ -42,8 +23,5 @@ onMounted(() => {
 </script>
 
 <template>
-  <p>TOKEN</p>
-  <p>{{ messagingStore.token }}</p>
-  <p>1645</p>
   <router-view />
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,12 @@
 <script setup>
 import ProductList from '@/components/ProductList.vue';
+import { useMessagingStore } from '@/stores/messaging'
+const messagingStore = useMessagingStore()
 </script>
 
 <template>
+  <p>TOKEN</p>
+  <p>{{ messagingStore.token }}</p>
+  <p>1645</p>
   <product-list />
 </template>
