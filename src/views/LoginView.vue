ae8b5de6ca6733b29b3c6353f65ca01e989bc958
Eduardo da Silva <eduardo.silva@gmail.com>
2024-05-27 10:08:44 -0300

    Add login header and change script to setup mode

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,10 +1,13 @@
-<script>
+<script setup>
 import '@passageidentity/passage-elements/passage-auth'
 
 
 </script>
 <template>
+  <h1>Login</h1>
   <div class="authContainer">
+    <!-- <label for="email">Email</label>
+    <input type="email" v-model="user.email" id="email"> -->
     <passage-auth app-id="9o2FumJTyoxmXOtHzjTdkOo9"></passage-auth>
   </div>
 </template>
