8cf5d17e2debaa214a1d35f5cf35cb9c6b1761bb
Eduardo da Silva <eduardo.silva@gmail.com>
2024-05-27 10:49:31 -0300

    Login View with form

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,7 @@
 <script setup>
 // import '@passageidentity/passage-elements/passage-auth'
 import axios from 'axios';
-import { reactive, ref } from 'vue'
+import { reactive, ref, onMounted } from 'vue'
 
 const user = reactive({
   email: 'admin@admin.com',
@@ -10,11 +10,32 @@ const user = reactive({
 
 const token = ref('')
 
+onMounted(async () => {
+  checkLocalLoginData()
+})
+
+const checkLocalLoginData = async () => {
+  console.log('oi')
+  const cred = await navigator.credentials.get()
+  console.log(cred)
+  if (cred) {
+    user.email = cred.id
+    user.password = cred.password
+    login()
+  }
+}
+
 const login = async () => {
   console.log('login')
   try {
     const { data } = await axios.post('https://web-6uo05kdzu892.up-de-fra1-k8s-1.apps.run-on-seenode.com/token/', user)
     token.value = data.access
+    const cred = new PasswordCredential({
+      id: user.email,
+      name: user.email,
+      password: user.password,
+    })
+    await navigator.credentials.store(cred)
   } catch (error) {
     token.value = error
   }
@@ -28,13 +49,15 @@ const login = async () => {
 
 </script>
 <template>
-  <h1>Login</h1>
+  <h1>Login with form</h1>
   <div class="authContainer">
-    <label for="email">Email</label>
-    <input type="email" v-model="user.email" id="email">
-    <label for="email">Senha</label>
-    <input type="password" v-model="user.password" id="email">
-    <button @click="login">Login</button>
+    <form @submit.prevent="() => { }">
+      <label for="email">Email</label>
+      <input type="email" v-model="user.email" id="email">
+      <label for="password">Senha</label>
+      <input type="password" v-model="user.password" id="password">
+      <button @click="login">Login</button>
+    </form>
   </div>
   Token: {{ token }}
 </template>
